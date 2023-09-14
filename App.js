29b8ab4b4fac1c6108b6a4001db8db2323d379e0
FelipeVeigaVellosoDaSilva <felipe.veiga.trabalho@gmail.com>
2023-09-14 17:52:51 -0300

    Conexão com banco de dados

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import Login from './Login.js'; // Substitua 'Login' pelo nome correto do seu arquivo Login.js
+// import Login from './Login.js'; // Substitua 'Login' pelo nome correto do seu arquivo Login.js
 import AppScreen from './TelaPrincipal.js'; // Substitua 'AppScreen' pelo nome correto do seu arquivo App.js
 
 const Stack = createStackNavigator();
@@ -10,7 +10,7 @@ const App = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Login">
-        <Stack.Screen name="Login" component={Login} />
+        {/* <Stack.Screen name="Login" component={Login} /> */}
         <Stack.Screen name="App" component={AppScreen} />
       </Stack.Navigator>
     </NavigationContainer>
--- Database.js
+++ Database.js
@@ -1,94 +1,115 @@
 import SQLite from 'react-native-sqlite-storage';
 
+
 // Abre ou cria o banco de dados
-const db = SQLite.openDatabase(
-  { name: 'products.db', location: 'default' },
-  () => {},
-  (error) => {
-    console.error('Erro ao abrir o banco de dados:', error);
-  }
-);
+export const createDatabase = () => {
+    const db = SQLite.openDatabase(
+        { name: 'products.db', location: 'default' },
+        () => { db = db; }, // Certifique-se de atribuir db = db aqui
+        (error) => {
+            console.error('Erro ao abrir o banco de dados:', error);
+        }
+    );
+    return db; // Retorne o banco de dados aberto
+};
+
 
 // Cria a tabela de produtos se ela não existir
-db.transaction((tx) => {
-  tx.executeSql(
-    `CREATE TABLE IF NOT EXISTS products (
-      id INTEGER PRIMARY KEY AUTOINCREMENT,
-      name TEXT NOT NULL,
-      price REAL NOT NULL,
-      quantity INTEGER NOT NULL
-    );`,
-    [],
-    () => {},
-    (error) => {
-      console.error('Erro ao criar a tabela de produtos:', error);
-    }
-  );
-});
+export const createTable = (db) => {
+    db.transaction((tx) => {
+        tx.executeSql(
+            `CREATE TABLE IF NOT EXISTS products (
+          id INTEGER PRIMARY KEY AUTOINCREMENT,
+          name TEXT NOT NULL,
+          price REAL NOT NULL,
+          quantity INTEGER NOT NULL
+        );
+        
+        INSERT INTO PRODUCTS (name, price, quantity) VALUES('teste', 12, 10);
+
+        `,
+            [],
+            () => {
+                console.log('Tabela de produtos criada com sucesso');
+            },
+            (error) => {
+                console.error('Erro ao criar a tabela de produtos:', error);
+            }
+        );
+    });
+};
 
 // Função para adicionar um novo produto
-const addProduct = (name, price, quantity, successCallback, errorCallback) => {
-  db.transaction((tx) => {
-    tx.executeSql(
-      'INSERT INTO products (name, price, quantity) VALUES (?, ?, ?)',
-      [name, price, quantity],
-      (_, result) => {
-        successCallback(result);
-      },
-      (_, error) => {
-        errorCallback(error);
-      }
-    );
-  });
+export const addProduct = (name, price, quantity, successCallback, errorCallback) => {
+    db.transaction((tx) => {
+        tx.executeSql(
+            'INSERT INTO products (name, price, quantity) VALUES (?, ?, ?)',
+            [name, price, quantity],
+            (_, result) => {
+                successCallback(result);
+            },
+            (_, error) => {
+                errorCallback(error);
+            }
+        );
+    });
 };
 
 // Função para listar todos os produtos
-const listProducts = (successCallback, errorCallback) => {
-  db.transaction((tx) => {
-    tx.executeSql(
-      'SELECT * FROM products',
-      [],
-      (_, { rows }) => {
-        successCallback(rows.raw());
-      },
-      (_, error) => {
-        errorCallback(error);
-      }
-    );
-  });
-};
+// export const listProducts = (successCallback, errorCallback) => {
+//     db.transaction((tx) => {
+//         tx.executeSql('SELECT * FROM products', [], (tx, results) => {
+//             if (results && results.rows) { // Verifica se results e results.rows não são nulos
+//                 const len = results.rows.length;
+//                 const products = [];
+//                 for (let i = 0; i < len; i++) {
+//                     const row = results.rows.item(i);
+//                     products.push(row);
+//                 }
+//                 setProducts(products);
+//             }
+//         });
+//     });
+// };
 
 // Função para atualizar um produto
-const updateProduct = (id, name, price, quantity, successCallback, errorCallback) => {
-  db.transaction((tx) => {
-    tx.executeSql(
-      'UPDATE products SET name=?, price=?, quantity=? WHERE id=?',
-      [name, price, quantity, id],
-      (_, result) => {
-        successCallback(result);
-      },
-      (_, error) => {
-        errorCallback(error);
-      }
-    );
-  });
+export const updateProduct = (id, name, price, quantity, successCallback, errorCallback) => {
+    db.transaction((tx) => {
+        tx.executeSql(
+            'UPDATE products SET name=?, price=?, quantity=? WHERE id=?',
+            [name, price, quantity, id],
+            (_, result) => {
+                successCallback(result);
+            },
+            (_, error) => {
+                errorCallback(error);
+            }
+        );
+    });
 };
 
 // Função para excluir um produto
-const deleteProduct = (id, successCallback, errorCallback) => {
-  db.transaction((tx) => {
-    tx.executeSql(
-      'DELETE FROM products WHERE id=?',
-      [id],
-      (_, result) => {
-        successCallback(result);
-      },
-      (_, error) => {
-        errorCallback(error);
-      }
-    );
-  });
+export const deleteProduct = (id, successCallback, errorCallback) => {
+    db.transaction((tx) => {
+        tx.executeSql(
+            'DELETE FROM products WHERE id=?',
+            [id],
+            (_, result) => {
+                successCallback(result);
+            },
+            (_, error) => {
+                errorCallback(error);
+            }
+        );
+    });
 };
-export default Database;
 
-export { addProduct, listProducts, updateProduct, deleteProduct };
+// Exporte as funções como um objeto
+export default {
+    createDatabase,
+    createTable,
+    addProduct,
+    updateProduct,
+    deleteProduct
+};
+
