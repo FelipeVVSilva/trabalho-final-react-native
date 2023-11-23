c8770abe9518715274f9699d274d30640010fc7a
FelipeVeigaVellosoDaSilva <felipe.veiga.trabalho@gmail.com>
2023-11-23 11:10:26 -0300

    Fazendo modificações para abrir app na web

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { AppRegistry } from 'react-native';  // Importe AppRegistry
+
 import Login from './Login.js';
 import AppScreen from './TelaCadastro.js';
 import Consulta from './Consulta';
@@ -10,7 +12,7 @@ const Stack = createStackNavigator();
 const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Login">
+      <Stack.Navigator initialRouteName="Login" component={Login}>
         <Stack.Screen name="Login" component={Login} />
         <Stack.Screen name="Cadastro" component={AppScreen} />
         <Stack.Screen name="Consulta" component={Consulta} />
@@ -18,5 +20,10 @@ const App = () => {
     </NavigationContainer>
   );
 };
+
+const appName = 'controle-estoque';
+// Registre o componente usando AppRegistry
+AppRegistry.registerComponent(appName, () => App);
+
 
 export default App;
